cb03ba004eae9fa7b5286686a1229f36a98fd1ed
R. Miles McCain <github@sendmiles.email>
2022-04-07 19:14:59 -0700

    work

--- labs/2-ir/code/tsop.c
+++ labs/2-ir/code/tsop.c
@@ -6,20 +6,41 @@
 #include "rpi.h"
 enum { ir_eps = 200 };
 
+#define IN_PIN 21
+
 // we should never get this.
 enum { NOISE = 0 } ;
 
-struct readings { unsigned usec, v; };
+typedef struct readings { unsigned usec, v; } reading;
 
 const char *key_to_str(unsigned x) {
-    output("convert an input value to a string\n");
-    unimplemented();
+    switch (x) {
+        case 0xc3de23dc: return "PLUS";
+        case 0xc3dfc23c: return "MINUS";
+        case 0xc3de5da2: return "REWIND";
+        case 0xc3df5ca2: return "PLAY/PAUSE";
+        case 0xc3df9c62: return "FORWARD";
+        case 0xc3dedd22: return "REPEAT";
+        case 0xc3dee31c: return "MUTE";
+        case 0xc3dea35c: return "SOURCE";
+        case 0xc3dfe21c: return "POWER";
+        default: return 0;
+    }
 }
 
 // adapt your read_while_equal: return 0 if timeout passed, otherwise
 // the number of microseconds + 1 (to prevent 0).
 static int read_while_eq(int pin, int v, unsigned timeout) {
-    unimplemented();
+    unsigned rb = timer_get_usec();
+    while (1) {
+        unsigned ra = timer_get_usec();
+        if ((ra - rb) >= timeout) {
+            return 0;
+        }
+        if (gpio_read(IN_PIN) != v) {
+            return ra - rb;
+        }
+    }
 }
 
 // integer absolute value.
@@ -29,21 +50,22 @@ static int abs(int x) {
 
 // return 0 if e is closer to <lb>, 1 if its closer to <ub>
 static int pick(struct readings *e, unsigned lb, unsigned ub) {
-    unimplemented();
-    panic("invalid time: <%d> expected %d or %d\n", e->usec, lb, ub);
+    int len = e->usec;
+    unsigned lb_len = abs((int)lb - len);
+    unsigned ub_len = abs((int)ub - len);
+    return ub_len < lb_len ? 0 : 1;
+
+    // panic("invalid time: <%d> expected %d or %d\n", e->usec, lb, ub);
 }
 
 // return 1 if is a skip: skip = delay of 550-/+eps
 static int is_skip(struct readings *e) {
-    unimplemented();
-    return 0;
+    return (abs((int)e->usec - (int)600) < 200) && e->v == 0;
 }
 
 // header is a delay of 9000 and then a delay of 4500
 int is_header(struct readings *r, unsigned n) {
-    if(n < 2)
-        return 0;
-    unimplemented();
+    return (r[n].usec > 3750) && (r[n].v == 0) && (r[n+1].usec > 3750) && (r[n+1].v == 1);
 }
 
 // convert <r> into an integer by or'ing in 0 or 1 depending on the 
@@ -51,7 +73,30 @@ int is_header(struct readings *r, unsigned n) {
 //
 // assert that they are seperated by skips!
 unsigned convert(struct readings *r, unsigned n) {
-    unimplemented();
+    printk("converting %d readings...\n");
+    uint32_t val = 0;
+    int start = 0;
+    for(int i = 0; i < n - 1; i++) {
+        if(is_header(r, i)) {
+            start = i + 3;
+        }
+    }
+    assert(start > 0);
+    printk("converting n = %d\n", n - start);
+    for(int i = start; i < n; i += 2) {
+        if (is_header(r, i)) {
+            break;
+        }
+
+        val |= pick(&r[i], 600, 1600);
+        val <<= 1;
+        // printk("    %d = %d for %d\n", i, r[i].v, r[i].usec);
+        if (i < (n - 1)) {
+            // printk("    %d = %d for %d\n", i+1, r[i+1].v, r[i+1].usec);
+            assert(is_skip(&r[i + 1]));
+        }
+    }
+    return val;
 }
 
 static void print_readings(struct readings *r, int n) {
@@ -71,13 +116,26 @@ static void print_readings(struct readings *r, int n) {
 
 // read in values until we get a timeout, return the number of readings.  
 static int get_readings(int in, struct readings *r, unsigned N) {
-    unimplemented();
+    int n = 0;
+    while(n < N) {
+        int val = gpio_read(in);
+        int dur = read_while_eq(in, val, 100000);
+        if (dur == 0) {
+            return n;
+        } else {
+            r[n].usec = dur;
+            r[n].v = val;
+        }
+        n++;
+    }
+    return n;
 }
 
 // initialize the pin.
 int tsop_init(int input) {
     // is open hi or lo?  have to set pullup or pulldown
-    unimplemented();
+    gpio_set_input(IN_PIN);
+    gpio_set_pullup(IN_PIN);
     return 1;
 }
 
@@ -89,8 +147,7 @@ void notmain(void) {
     // very dumb starter code
     while(1) {
         // wait until signal: or should we timeout?
-        while(gpio_read(in))
-            ;
+        while(gpio_read(in)) {}
 #       define N 256
         static struct readings r[N];
         int n = get_readings(in, r, N);
@@ -104,7 +161,8 @@ void notmain(void) {
             printk("%s\n", key);
         else
             // failed: dump out the data so we can figure out what happened.
-            print_readings(r,n);
+            // print_readings(r,n);
+            {}
     }
 	printk("stopping ir send/rec!\n");
     clean_reboot();
