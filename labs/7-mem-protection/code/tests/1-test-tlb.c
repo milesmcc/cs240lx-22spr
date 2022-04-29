7c356eaff4d2cdcc452a38ca1e22c8422f67c518
R. Miles McCain <github@sendmiles.email>
2022-04-28 17:24:34 -0700

    add extra test

--- labs/7-mem-protection/code/tests/1-test-tlb.c
+++ labs/7-mem-protection/code/tests/1-test-tlb.c
@@ -1,6 +1,7 @@
+/* Miles' little test for tlb_contains_va */
+
 #include "rpi.h"
 #include "pinned-vm.h"
-#include "assert.h"
 
 void notmain(void) { 
     // map the heap: for lab cksums must be at 0x100000.
@@ -14,8 +15,11 @@ void notmain(void) {
     output("hello: mmu must be on\n");
 
     uint32_t result = 0;
-    demand(tlb_contains_va(&result, p.map[2].addr + 0xFAA0), "did not contai valid VA");
-    demand(result == p.map[2].addr + 0xFAA0, "VA address didn't match!");
+
+    for(int i = 0; i < p.n; i++) {
+        demand(tlb_contains_va(&result, p.map[i].addr + 0xFAA0), "did not contai valid VA");
+        demand(result == p.map[i].addr + 0xFAA0, "VA address didn't match!");
+    }
 
     demand(!tlb_contains_va(&result, 0xDEADFAA0), "contained invalid VA");
 
