fc38b117d14c5e84fa2a0777bd9b80f341dfc21e
R. Miles McCain <github@sendmiles.email>
2022-05-03 20:34:10 -0700

    part 2

--- labs/9-memcheck-stat/code/ck-memcheck.part1.c
+++ labs/9-memcheck-stat/code/ck-memcheck.part1.c
@@ -5,9 +5,6 @@
 #include "rpi-armtimer.h"
 #include "ckalloc.h"
 
-typedef struct trampoline_t {
-    unsigned push, bl, pop, inst, ret;
-} trampoline_t;
 
 // you'll need to pull your code from lab 2 here so you
 // can fabricate jumps
@@ -25,52 +22,6 @@ static uint32_t
     // you will have to define these functions.
     start_nocheck = (uint32_t)ckalloc_start,
     end_nocheck = (uint32_t)ckalloc_end;
-
-static int should_overwrite(uint32_t instruction) {
-    return 0xe5000000 == (instruction & 0xff000000) && // Is a load or store
-        !((0x000f0000 & instruction ) != 0x000f0000); // Does not involve pc
-}
-
-static inline uint32_t arm_b(uint32_t from, uint32_t to) {
-    int32_t delta = (to - (from + 8)) / 4;
-    return 0xea000000 | ((delta) & (0xffffff));
-}
-
-static inline uint32_t arm_bl(uint32_t from, uint32_t to) {
-    int32_t delta = (to - (from + 8)) / 4;
-    return 0xeb000000 | ((delta) & (0xffffff));
-}
-
-static int check_heap_wrapper() {
-    printk("[Call to check_heap_wrapper!]\n");
-    return ck_heap_errors();
-}
-
-static void _trampoline_playground(void) {
-    asm volatile("push {r0-r12, lr}");
-    ck_heap_errors();
-    asm volatile("pop {r0-r12, lr}");
-    asm volatile("nop");
-}
-
-static void overwrite(uint32_t pc) {
-    static trampoline_t *trampoline_vector = 0;
-    if (trampoline_vector == 0) {
-        trampoline_vector = kmalloc(100000);
-    }
-
-    trampoline_t *t = &trampoline_vector[pc];
-
-    // Generate trampoline
-    t->push = 0xe92d5fff;
-    t->bl = arm_bl((uint32_t) &t->bl, (uint32_t) check_heap_wrapper);
-    t->pop = 0xe8bd5fff;
-    t->inst = *(uint32_t *)pc;
-    t->ret = arm_b((uint32_t) &t->ret, pc + 4);
-
-    // Overwrite original instruction
-    *(uint32_t *)pc = arm_b(pc, (uint32_t) t);
-}
 
 static int in_range(uint32_t addr, uint32_t b, uint32_t e) {
     assert(b<e);
@@ -110,11 +61,10 @@ static inline void ck_mem_interrupt(uint32_t pc) {
     PUT32(arm_timer_IRQClear, 1);
 
     if (ck_mem_checked_pc(pc)) {
-        if (should_overwrite(*(uint32_t *)pc)) {
-            overwrite(pc);
-            printk("***** Overwrote %p!\n", pc);
-        }
         checked++;
+        if (ck_heap_errors()) {
+            errors++;
+        }
     } else {
         skipped++;
     }
