cf5a6a4b8ba5d8b006f3f6c1da5d88092423e3bf
R. Miles McCain <github@sendmiles.email>
2022-05-19 19:19:28 -0700

    rx

--- labs/13-verilog-uart/1-rx/counter.sv
+++ labs/13-verilog-uart/1-rx/counter.sv
@@ -7,15 +7,17 @@ module counter
         input  logic clk, rst,
         output logic max_tick
     );
-    logic [31:0] count;
+    logic [($clog2(M)-1):0] count;
 
     always_ff @(posedge clk, posedge rst)
         if (rst)
             count <= '0;
+        /* verilator lint_off WIDTH */
         else if (count >= M)
             count <= '0;
         else
             count <= (count + 1);
 
+    /* verilator lint_off WIDTH */
     assign max_tick = count == M - 1;
 endmodule
--- labs/13-verilog-uart/1-rx/uart_rx.sv
+++ labs/13-verilog-uart/1-rx/uart_rx.sv
@@ -13,64 +13,66 @@ module uart_rx
     state_t state_reg = idle;
     state_t state_next;
 
-    logic [7:0] dout_reg;
     logic [7:0] dout_next;
     
-    logic [4:0] dout_index_reg;
-    logic [4:0] dout_index_next;
-    logic [11:0] ticks;
+    logic [2:0] dout_index_reg;
+    logic [2:0] dout_index_next;
+    
+    logic [3:0] ticks_reg;
+    logic [3:0] ticks_next;
+
+    logic rx_done_next;
 
     always_ff @(posedge clk, posedge rst)
         if (rst) begin
             state_reg <= idle;
-            ticks <= '0;
             rx_done_tick <= '0;
+            ticks_reg <= '0;
         end else begin
             state_reg <= state_next;
             dout_index_reg <= dout_index_next;
-            dout_reg <= dout_next;
-            if (state_next == idle) begin
-                ticks <= '0;
-                dout_index_reg <= '0;
-                rx_done_tick <= '0;
-            end else if (state_next == data) begin
-                
-            end
-            if (state_next == stop) begin
-                dout <= dout_next;
-                rx_done_tick <= '1;
-            end
-            if (tick)
-                ticks <= ticks + 1;
+            dout <= dout_next;
+            ticks_reg <= ticks_next;
+            rx_done_tick <= rx_done_next;
+            dout <= dout_next;
         end
 
     always_comb begin
         state_next = state_reg;
-        dout_next = dout_reg;
+        dout_next = dout;
         dout_index_next = dout_index_reg;
+        rx_done_next = rx_done_tick;
+        ticks_next = ticks_reg;
 
         if (tick) begin
+            ticks_next = ticks_reg + 1;
+
             unique case (state_reg)
                 idle: begin
                     if (!rx)
                         state_next = start;
+                        ticks_next = '0;
                 end
                 start: begin
-                    if (ticks > 16) begin
+                    if (ticks_reg == 15) begin
                         state_next = data;
-                        dout_next = 0;
+                        dout_next = '0;
+                        rx_done_next = '0;
+                        dout_index_next = '0;
                     end
                 end
                 data: begin
-                    if (ticks % 16 == 8) begin
-                        dout_next = {rx,dout_reg[7:1]};
+                    if (ticks_reg == 7) begin
+                        dout_next = {rx,dout[7:1]};
                         dout_index_next = dout_index_reg + 1;
-                        if (dout_index_next == 8)
+                        if (dout_index_reg == 7) begin
                             state_next = stop;
+                            rx_done_next = '1;
+                        end
                     end
                 end
                 stop: begin
-                    if (ticks % 16 == 7) begin
+                    if (ticks_reg == 7) begin
                         state_next = idle;
                     end
                 end
